410cf9861e4bb0a02c53a494fe7bc8bc24ef0b08
Matthias Valvekens <matthias@mvalvekens.be>
2022-09-02 09:05:10 +0200

    Removed unused parts of cabal and stack files

--- test/Spec.hs
+++
@@ -1,8 +0,0 @@
-import Test.HUnit
-import Test.ASN1.Core
-import Test.ASN1.UniversalTypes
-
-main :: IO ()
-main = do
-    cts <- runTestTT $ test [coreParsingTests, universalParsingTests]
-    putStrLn $ show cts
